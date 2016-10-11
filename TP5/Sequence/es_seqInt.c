9b99cc78988b87f292f73bb0c9b6ed736614a809
DrEtRhI <thierryeric@free.fr>
2016-10-11 21:49:26 +0200

    161011-2149_TP5/Sequence

---
+++ TP5/Sequence/es_seqInt.c
@@ -0,0 +1,12 @@
+#include "type_seqInt.h"
+
+void LireSeq(SeqInt *S){
+	printf("Entrez un entier : \n");
+	scanf("%d", &((*S).T[0]));
+	printf("Entrez un autre entier : \n");
+	scanf("%d", &((*S).T[1]));
+}
+
+void EcrireSeq(SeqInt S){
+	
+}
