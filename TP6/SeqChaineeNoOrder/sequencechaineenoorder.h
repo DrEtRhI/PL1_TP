92da2b24c58689c5f6ba5b23af343dc7f4c0586f
DrEtRhI <thierryeric@free.fr>
2016-11-08 09:07:19 +0100

    commentaires dans les .h

--- TP6/SeqChaineeNoOrder/sequencechaineenoorder.h
+++ TP6/SeqChaineeNoOrder/sequencechaineenoorder.h
@@ -3,14 +3,30 @@
 typedef struct cellule{
 	int E ;  /* champ valeur de la cellule */
 	struct cellule *suiv ;    /* champ adresse de la cellule suivante */
-} Cellule ;
+} Cellule ;  /*Nom de l'objet élémentaire constituant la liste chainée*/
 
-typedef Cellule *SeqInt; /* definition du type pointeur de SeqInt */
+typedef Cellule *SeqInt; /* definition du type pointeur de Cellule : SeqInt */
 
 /* en-tetes des fonctions sur SeqInt */
 
+
+/* Création d'une liste chainée entrée au clavier. La taille de la séquence
+   est demandée, puis les valeurs formant cette séquence. Les nouveaux
+   élément créés sont chainée en tete.*/
 void LireSeq (SeqInt *s) ;
+
+/* Affiche à l'écran les valeurs contenues dans la liste chainée.*/
 void EcrireSeq (SeqInt s) ;
+
+/* Cherche le premier entier x de la séquence. La fonction renvoie vrai(1)
+   si x est présent dans la séquence chainée, sinon elle renvoie faux(0).*/
 int EstPresent (SeqInt s, int x) ;
+
+/* Insère le nouvellement élément ayant pour valeur x en tete de la séquence
+   chainée.*/
 void Inserer (SeqInt *s, int x) ;
+
+/* Supprime l'élément ayant pour valeur x. Si x n'est pas présent dans la 
+   séquence chainée alors elle est inchangée. S'il est présent l'élément est
+   déconnecté de la séquence et la case mémoire est libérée.*/
 void Supprimer (SeqInt *s, int x) ;
--- TP6/SeqContNoOrder/sequencecontnoorder.h
+++ TP6/SeqContNoOrder/sequencecontnoorder.h
@@ -1,5 +1,13 @@
 
-/* Type Sequence (representation contigue dans un tableau) */
+/* Type Sequence (representation contigue dans un tableau) 
+	 Création de fonctions et action opérant sur des séquences
+	 contigues dans un tableau :
+	 Création séquence
+	 Affichage séquence
+	 Recherche d'un élément dans la séquence
+	 Insérer un élément dans la séquence
+	 Supprimer un élément de la séquence
+	 */
 
 #define LGMAX 30
 
@@ -12,8 +20,27 @@ typedef struct {
 
 /* en-tetes des fonctions sur SeqInt */
 
+/* Création d'une séquence entrée séquentiellement par l'utilisateur
+	 Vérification préalable que la taille de la séquence demandé par 
+	 l'utilisateur est bien <= LGMAX.
+	 Tant que la longueur la taille de séquence demandée par l'utilisateur
+	 est supérieure à LGMAX, une nouvelle taille lui est demandée.*/
 void LireSeq (SeqInt *s) ;
+
+
+/* Affichage à l'écran d'une séquence éxistante */
 void EcrireSeq (SeqInt s) ;
+
+/* Recherche dans une séquence le premier entier x. Si x est présent dans
+	 la séquence la fonction renvoie vrai(1) sinon la fonction renvoie 
+	 faux(0).*/
 int EstPresent (SeqInt s, int x) ;
+
+/* Insère en queue de séquence le nouvel entier x*/ 
 void Inserer (SeqInt *s, int x) ;
+
+/* Supprime l'entier x de la séquence s'il est présent. La valeur du dernier
+   entier de la séquence est copié à l'indice de x. Et la taille de la
+   séquence est réduite de 1. Si x n'est pas présent dans la séquence, cette
+   dernière est inchangée.*/ 
 void Supprimer (SeqInt *s, int x) ;
